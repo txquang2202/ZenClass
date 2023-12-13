859a2d83fc17f6cf2f27755a980a45ea84c377a8
HQDuy47 <72744215+HQDuy47@users.noreply.github.com>
2023-12-13 15:31:37 +0700

    add join class

--- frontEnd/src/components/SideBar/SideBar.js
+++ frontEnd/src/components/SideBar/SideBar.js
@@ -8,9 +8,11 @@ import { useClassContext } from "../../context/ClassContext";
 import { createClass } from "../../services/classServices";
 import { jwtDecode } from "jwt-decode";
 import { toast } from "react-toastify";
+import { Menu, MenuItem } from "@material-ui/core";
 
 function SideBar() {
   const { id } = useParams();
+  const [anchorEl, setAnchorEl] = useState(null);
   const [activeLink, setActiveLink] = useState("main");
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [newClassInfo, setNewClassInfo] = useState({
@@ -19,8 +21,11 @@ function SideBar() {
     className: "",
   });
   const token = localStorage.getItem("token");
+  const [joinModalIsOpen, setJoinModalIsOpen] = useState(false);
+  const [joinClassCode, setJoinClassCode] = useState("");
   let data;
   if (token) data = jwtDecode(token);
+
   const handleLinkClick = (link) => {
     setActiveLink(link);
   };
@@ -28,6 +33,7 @@ function SideBar() {
   const { addClass } = useClassContext();
 
   const openModal = () => {
+    handleClose();
     setModalIsOpen(true);
   };
 
@@ -53,74 +59,67 @@ function SideBar() {
     }
   };
 
+  const openJoinModal = () => {
+    handleClose();
+    setJoinModalIsOpen(true);
+  };
+
+  const closeJoinModal = () => {
+    setJoinModalIsOpen(false);
+  };
+
+  const handleJoinClass = async () => {
+    try {
+      // Implement the logic to join a class using joinClass function
+      // Use the joinClass function similar to how createClass is used
+      // Example: const response = await joinClass(joinClassCode, token);
+      // Handle the response as needed
+      // ...
+
+      // Close the modal and reset the state
+      closeJoinModal();
+      toast.success("Joined the class successfully!");
+      setJoinClassCode("");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className="w-1/5 md:w-1/6 lg:w-1/12 p-4 rounded-lg border border-solid border-gray-200 pb-96 ">
       <ul className="space-y-8 text-center">
-        <li className="">
+        <li>
           <button
-            onClick={openModal}
+            onClick={handleMenu}
             className="btn bg-[#2E80CE] text-white px-3 py-1 lg:px-4 lg:py-1 rounded-full text-2xl cursor-pointer hover:bg-blue-400 shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
           >
             +
           </button>
-
-          {/* Modal */}
-          <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            contentLabel="Create Class Modal"
-            // className="h-36 w-[400px] hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center  md:inset-0  "
-            className="h-36 w-[400px] absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
-            // overlayClassName="overlay"
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            anchorOrigin={{
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+            }}
+            className="mt-12"
           >
-            <div className="bg-white p-8 rounded-md border-solid border-2 border-gray-200">
-              <h2 className="text-2xl font-semibold mb-4">Create New Class</h2>
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-600">
-                  Title:
-                </label>
-                <input
-                  type="text"
-                  value={newClassInfo.title}
-                  onChange={(e) =>
-                    setNewClassInfo({ ...newClassInfo, title: e.target.value })
-                  }
-                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                />
-              </div>
-              <div className="mb-6">
-                <label className="block text-sm font-medium text-gray-600">
-                  Class Name:
-                </label>
-                <input
-                  type="text"
-                  value={newClassInfo.className}
-                  onChange={(e) =>
-                    setNewClassInfo({
-                      ...newClassInfo,
-                      className: e.target.value,
-                    })
-                  }
-                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                />
-              </div>
-              <div className="flex justify-end">
-                <button
-                  onClick={handleCreateClass}
-                  className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
-                >
-                  Create Class
-                </button>
-                <button
-                  onClick={closeModal}
-                  className="border border-gray-300 px-4 py-2 rounded-md"
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </Modal>
-          {/* End Modal */}
+            <MenuItem onClick={openModal}>Create Class</MenuItem>
+            <MenuItem onClick={openJoinModal}>Join Class</MenuItem>
+          </Menu>
         </li>
         <li>
           <Link
@@ -165,6 +164,102 @@ function SideBar() {
           </Link>
         </li>
       </ul>
+
+      {/* Modal Create Class*/}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Create Class Modal"
+        // className="h-36 w-[400px] hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center  md:inset-0  "
+        className="h-36 w-[400px] absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+        // overlayClassName="overlay"
+      >
+        <div className="bg-white p-8 rounded-md border-solid border-2 border-gray-200">
+          <h2 className="text-2xl font-semibold mb-4">Create New Class</h2>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Title:
+            </label>
+            <input
+              type="text"
+              value={newClassInfo.title}
+              onChange={(e) =>
+                setNewClassInfo({ ...newClassInfo, title: e.target.value })
+              }
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-sm font-medium text-gray-600">
+              Class Name:
+            </label>
+            <input
+              type="text"
+              value={newClassInfo.className}
+              onChange={(e) =>
+                setNewClassInfo({
+                  ...newClassInfo,
+                  className: e.target.value,
+                })
+              }
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              onClick={handleCreateClass}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
+            >
+              Create Class
+            </button>
+            <button
+              onClick={closeModal}
+              className="border border-gray-300 px-4 py-2 rounded-md"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Modal>
+      {/* End Modal */}
+
+      {/* Join Class Modal */}
+      <Modal
+        isOpen={joinModalIsOpen}
+        onRequestClose={closeJoinModal}
+        contentLabel="Join Class Modal"
+        className="h-36 w-[400px] absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 "
+      >
+        <div className="bg-white p-8 rounded-md border-solid border-2 border-gray-200">
+          <h2 className="text-2xl font-semibold mb-4">Join a Class</h2>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-600">
+              Class Code:
+            </label>
+            <input
+              type="text"
+              value={joinClassCode}
+              onChange={(e) => setJoinClassCode(e.target.value)}
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              onClick={handleJoinClass}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
+            >
+              Join Class
+            </button>
+            <button
+              onClick={closeJoinModal}
+              className="border border-gray-300 px-4 py-2 rounded-md"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Modal>
+      {/* End Join Class Modal */}
     </div>
   );
 }
